82a004fe0641dcacd139323d98f1dfaf5fc5e801
skril349 <tvives.cabaleiro@gmail.com>
2026-03-06 13:05:30 +0100

    adding hello world view

--- my_blog/posts/views.py
+++ my_blog/posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import re
 
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, View
 # Create your views here.
+
+class HelloWorld(View):
+    def get(self, request):
+        return render(request, 'helloworld.html', {'message': 'Hello World!'})
