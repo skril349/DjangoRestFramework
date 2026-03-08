3fb8b8b830e4fb4c646ceb6007b7fdf821561905
skril349 <tvives.cabaleiro@gmail.com>
2026-03-08 12:25:05 +0100

    adding posts to django admin

--- my_blog/posts/admin.py
+++ my_blog/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'order', 'created_at')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'description')
