82e5276fdf6590a76387f9ea6ad65518ccea02d0
skril349 <tvives.cabaleiro@gmail.com>
2026-03-08 17:42:38 +0100

    adding social media to user properties

--- my_blog/user/admin.py
+++ my_blog/user/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Información personal', {'fields': ('first_name', 'last_name', 'email',)}),
+        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
+        ('Redes sociales', {'fields': ('website','github','twitter')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'last_login')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+     
--- my_blog/user/models.py
+++ my_blog/user/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
-
+    website = models.CharField(max_length=255, blank=True, null=True)
+    twitter = models.CharField(max_length=255, blank=True, null=True)
+    github = models.CharField(max_length=255, blank=True, null=True)
+    def __str__(self):
+        return self.username
