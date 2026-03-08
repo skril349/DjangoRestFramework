9712edcad1f2380be8bcffc1ededefd5f0264862
skril349 <tvives.cabaleiro@gmail.com>
2026-03-08 16:13:07 +0100

    python manage.py collectstatic  , creating statics to generate documentation

--- my_blog/config/settings.py
+++ my_blog/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'posts'
 ]
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = './static/'
