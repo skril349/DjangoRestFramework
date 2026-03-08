c458b4464920223f76ab2b082c9b9cd7d2cbad6b
skril349 <tvives.cabaleiro@gmail.com>
2026-03-08 15:44:26 +0100

    creating CRUD complete

--- my_blog/posts/api/router.py
+++ my_blog/posts/api/router.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from posts.api.views import PostViewSet
+from posts.api.views import PostModelViewSet
 
 router_posts = DefaultRouter()
-router_posts.register(prefix='posts', viewset=PostViewSet, basename='posts')
+router_posts.register(prefix='posts', viewset=PostModelViewSet, basename='posts')
--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -1,7 +1,7 @@
 import re
 
 from rest_framework.views import APIView
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from posts.models import Post
@@ -9,29 +9,35 @@ from posts.api.serializers import PostSerializer
 
 
 
-
-class PostViewSet(ViewSet):
+class PostModelViewSet(ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
     
-    # get /api/posts
-    def list(self, request):
-        serializer = PostSerializer(Post.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
     
-    # get /api/posts/{id}
-    def retrieve(self, request, pk=int):
-        try:
-            post = PostSerializer(Post.objects.get(id=pk))
-            return Response(status=status.HTTP_200_OK, data=post.data)
-        except Post.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND, data={'message': 'Post not found'})
 
-    # post /api/posts
-    def create(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
-        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+
+# class PostViewSet(ViewSet):
+    
+#     # get /api/posts
+#     def list(self, request):
+#         serializer = PostSerializer(Post.objects.all(), many=True)
+#         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+#     # get /api/posts/{id}
+#     def retrieve(self, request, pk=int):
+#         try:
+#             post = PostSerializer(Post.objects.get(id=pk))
+#             return Response(status=status.HTTP_200_OK, data=post.data)
+#         except Post.DoesNotExist:
+#             return Response(status=status.HTTP_404_NOT_FOUND, data={'message': 'Post not found'})
+
+#     # post /api/posts
+#     def create(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+#         return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
     
     
 # class PostApiView(APIView):
