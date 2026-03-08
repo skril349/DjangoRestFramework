17b98ff8bffeba131d6a3da78e95ae9ab72ec981
skril349 <tvives.cabaleiro@gmail.com>
2026-03-08 12:55:47 +0100

    creating post serializers

--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -2,15 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from posts.models import Post
+from posts.api.serializers import PostSerializer
+
+
 class PostApiView(APIView):
     def get(self, request):
-        posts = [post.title for post in Post.objects.all()]
-        return Response(status=status.HTTP_200_OK, data=posts)
-    
+        serializer = PostSerializer(Post.objects.all(), many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
+
     def post(self, request):
-        Post.objects.create(
-            title = request.data.get('title'),
-            description = request.data.get('description'),
-            order = request.data.get('order')
-        )
-        return Response(status=status.HTTP_201_CREATED, data={'message': 'Post created successfully.'})
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+        
