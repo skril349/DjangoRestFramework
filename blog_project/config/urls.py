01145cbfbb5bc18c0285c9d6beceec4001c05e9a
skril349 <tvives.cabaleiro@gmail.com>
2026-03-10 09:39:55 +0100

    creating routes for swagger

--- blog_project/config/urls.py
+++ blog_project/config/urls.py
@@ -16,7 +16,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog Api",
+      default_version='v1',
+      description="Documentación de la API del Blog",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="tvives.cabaleiro@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
