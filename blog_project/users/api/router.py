08f1a39b57e88b6659f899de4ab67c26c17f974e
skril349 <tvives.cabaleiro@gmail.com>
2026-03-10 10:55:48 +0100

    adding routers to get me

--- blog_project/users/api/router.py
+++ blog_project/users/api/router.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserView
 
 # Creamos las rutas para los endpoints de registro y login de usuarios
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/me/', UserView.as_view(), name='user'),
+    
 ]
 
