6c7d7ce9bf51901d35b13f91f35ae8fe80ff5960
skril349 <tvives.cabaleiro@gmail.com>
2026-03-10 10:23:24 +0100

    adding jwt to acces and refresh token

--- blog_project/config/urls.py
+++ blog_project/config/urls.py
@@ -20,6 +20,8 @@ from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,4 +41,6 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('api/', include('users.api.router')),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
