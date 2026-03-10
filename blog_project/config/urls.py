7401a6e34e2dc7bb337b87ae339a724d61f5221c
skril349 <tvives.cabaleiro@gmail.com>
2026-03-10 10:12:53 +0100

    adding endpoint register to settings

--- blog_project/config/urls.py
+++ blog_project/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -38,4 +38,5 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
+    path('api/', include('users.api.router')),
 ]
