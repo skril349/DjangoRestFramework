ad1d22e9626e0b6f7c818cc4e102d1de80294490
skril349 <tvives.cabaleiro@gmail.com>
2026-03-10 10:55:32 +0100

    adding UserView to get me

--- blog_project/users/api/views.py
+++ blog_project/users/api/views.py
@@ -1,9 +1,12 @@
 # Creamos endpoints para el registro y login de usuarios
-from users.api.serializers import UserRegisterSerializer
+from posixpath import isabs
+
+from users.api.serializers import UserRegisterSerializer, UserSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from users.models import User
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
@@ -12,3 +15,9 @@ class RegisterView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
