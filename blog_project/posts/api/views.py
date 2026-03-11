819d360bc4fc3690da182ba649c8b22f1dceb476
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 10:25:47 +0100

    filtering category by slug

--- blog_project/posts/api/views.py
+++ blog_project/posts/api/views.py
@@ -2,10 +2,13 @@ from rest_framework.viewsets import ModelViewSet
 from posts.models import Post
 from .serializers import PostSerializer
 from posts.api.permissions import IsAdminOrReadOnly
-
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(published=True)
     lookup_field = "slug"
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category__slug']
+    
