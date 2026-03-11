67f6289344ecaf0d2dc1605e653cd7da09cf1e52
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 09:33:16 +0100

    filtering by published with django_filters

--- blog_project/categories/api/views.py
+++ blog_project/categories/api/views.py
@@ -1,14 +1,15 @@
-from codecs import lookup
-
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .serializers import CategorySerializer
 from categories.models import Category
 from categories.api.permissions import IsAdminOrReadOnly
 
+
 class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
-    #queryset = Category.objects.all()
-    queryset = Category.objects.filter(published=True) #solo mostrar categorias publicadas
-    lookup_field = 'slug' #buscar por slug en lugar de id
-    
+    queryset = Category.objects.all()
+    lookup_field = "slug"
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["published"]
