af2c1e0657747d4388bf8bb9632e4c7be28c0c17
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 10:49:12 +0100

    filtering by date creation

--- blog_project/comments/api/views.py
+++ blog_project/comments/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from comments.models import Comment
 from .serializers import CommentSerializer
+from rest_framework.filters import  OrderingFilter
 #from posts.api.permissions import IsAdminOrReadOnly
 
 class CommentApiViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['created_at']
     #permission_classes = [IsAdminOrReadOnly]
