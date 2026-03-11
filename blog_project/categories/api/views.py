a3f03e261cec5992fd02ba0d677f09bc81eb645d
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 09:22:40 +0100

    filtering published true and changing get by id to get by slug

--- blog_project/categories/api/views.py
+++ blog_project/categories/api/views.py
@@ -1,3 +1,5 @@
+from codecs import lookup
+
 from rest_framework.viewsets import ModelViewSet
 from .serializers import CategorySerializer
 from categories.models import Category
@@ -6,5 +8,7 @@ from categories.api.permissions import IsAdminOrReadOnly
 class CategoryApiViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     serializer_class = CategorySerializer
-    queryset = Category.objects.all()
+    #queryset = Category.objects.all()
+    queryset = Category.objects.filter(published=True) #solo mostrar categorias publicadas
+    lookup_field = 'slug' #buscar por slug en lugar de id
     
