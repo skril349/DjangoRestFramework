7b4d31b8de93c01cd6a5fc32019fd02885ca33a2
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 10:08:03 +0100

    getting user and category details for post

--- blog_project/posts/api/serializers.py
+++ blog_project/posts/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from posts.models import Post
+from users.api.serializers import UserSerializer
+from categories.api.serializers import CategorySerializer
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    category = CategorySerializer()
     class Meta:
         model = Post
         fields = ["id", "title", "content", "slug", "miniature", "published", "created_at", 'user', 'category']
