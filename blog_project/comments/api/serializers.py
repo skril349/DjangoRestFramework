4e94e4f9e528094c9acce2b4bf24cf1c82d2f8e7
skril349 <tvives.cabaleiro@gmail.com>
2026-03-11 10:47:59 +0100

    adding user and post serializer

--- blog_project/comments/api/serializers.py
+++ blog_project/comments/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework.serializers import ModelSerializer
 from comments.models import Comment
+from posts.api.serializers import PostSerializer
+from users.api.serializers import UserSerializer
 
 class CommentSerializer(ModelSerializer):
+    user = UserSerializer()
+    post = PostSerializer()
     class Meta:
         model = Comment
         fields = ['id', 'content', 'created_at', 'post', 'user']
